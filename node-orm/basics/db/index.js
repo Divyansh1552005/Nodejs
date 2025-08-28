c27ac8916bf8e614db9d117428372f4a33bc0488
Divyansh <officialdslc1552005@gmail.com>
2025-08-28 23:30:29 +0530

    ..

--- node-orm/basics/db/index.js
+++ node-orm/basics/db/index.js
@@ -1,20 +1,12 @@
-const {drizzle} = require("drizzle-orm/node-postgres")
+require('dotenv/config');
+const { drizzle } = require('drizzle-orm/node-postgres');
 
-
-// connection to db
-// postgress://<username>:<password>@<host>:<port>/<database_name>
-const { Pool } = require("pg");
-
-
-// connection to db
-// postgress://<username>:<password>@<host>:<port>/<database_name>
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || "postgres://postgres:admin@localhost:5432/mydb"
+// You can specify any property from the node-postgres connection options
+const db = drizzle({ 
+  connection: { 
+    connectionString: process.env.DATABASE_URL || "postgres://postgres:admin@localhost:5432/mydb"
+  }
 });
-
-const db = drizzle(pool);
-
-module.exports = db;
 
 module.exports = db;
 
