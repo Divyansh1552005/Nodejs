e9dce1fe3933cdba77e231bf6a044d5f9fa8bd88
Divyansh <officialdslc1552005@gmail.com>
2025-08-28 23:31:12 +0530

    resoleved some issues

--- node-orm/basics/db/index.js
+++ node-orm/basics/db/index.js
@@ -4,7 +4,7 @@ const { drizzle } = require('drizzle-orm/node-postgres');
 // You can specify any property from the node-postgres connection options
 const db = drizzle({ 
   connection: { 
-    connectionString: process.env.DATABASE_URL || "postgres://postgres:admin@localhost:5432/mydb"
+    connectionString: process.env.DATABASE_URL
   }
 });
 
