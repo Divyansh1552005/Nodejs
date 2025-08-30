fb6ec856fee4f1407075fa87d7e490ca97ca1e81
Divyansh <officialdslc1552005@gmail.com>
2025-08-30 19:32:44 +0530

    did a lot

--- Express/BookStore/models/manga.model.js
+++ Express/BookStore/models/manga.model.js
@@ -1,7 +1,7 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
 const booksTable = pgTable("books", {
-    id: integer("id").primaryKey().notNull(),
+    id: integer("id").primaryKey().notNull().unique(),
     title: varchar("title", { length: 255 }).notNull(),
     author: varchar("author", { length: 255 }).notNull(),
     year: integer("year").notNull()
