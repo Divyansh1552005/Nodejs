5ae70bb591bd0c21b1b2af1cd24a87e3ab9bfd9c
Divyansh <officialdslc1552005@gmail.com>
2025-09-28 13:33:49 +0530

    learned dockerizing nodejs applications

---
+++ Docker/Dockerize_apps/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World! This is a Dockerized Node.js application.");
+});
+
+app.get("/api/data", (req, res) => {
+  res.json({ message: "This is some sample data from the API." });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+
+/*
+    Let us list all the requirements this file has:
+    1. Nodejs
+    2. npm
+    3. express
+    
+
+*/
